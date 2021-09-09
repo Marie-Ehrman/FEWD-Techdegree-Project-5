ff085fb7cc0dc83a76bdca33db76a5c9a31b38a4
Marie-Ehrman <marie.e.ehrman@gmail.com>
2021-09-09 11:41:34 -0700

    html string bug on line 24

--- js/app.js
+++ js/app.js
@@ -1,6 +1,30 @@
+//Load gallery using baguetteBox
 window.addEventListener('load', function() {
     baguetteBox.run('.gallery');
   });
 
+// const search = new Filter('search', 'data-caption');
 
-const search = new Filter('search', 'data-caption');
+//  VARIABLES
+const gallery = document.querySelector('.gallery')
+const photos = gallery.children
+console.log(photos)
+
+const searchInput = document.querySelector('#search');
+
+//SEARCH INPUT EVENT LISTENER
+searchInput.addEventListener('keyup', e => {
+  let matches = '';
+
+  for (let i = 0; i < photos.length; i++){
+
+    const caption = photos[i].getAttribute('data-caption');
+    if(caption.includes(e.target.value)){
+      console.log(photos[i])
+      matches += `${photos[i]}`
+    }     
+    console.log(matches)
+  }
+  
+// gallery.innerHTML = matches
+});
