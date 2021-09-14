56f64ab43aaa8045a439fd0ca8f99e428be1cdda
Marie-Ehrman <marie.e.ehrman@gmail.com>
2021-09-14 12:39:59 -0700

    grid not working

--- js/app.js
+++ js/app.js
@@ -10,16 +10,34 @@ const gallery = document.querySelector('.gallery')
 const photos = gallery.children
 const searchInput = document.querySelector('#search');
 
+
 //SEARCH INPUT EVENT LISTENER
 searchInput.addEventListener('keyup', e => {
+  let noMatches = false;
 
   for (let i = 0; i < photos.length; i++){
+      
+      const userInput = e.target.value.toLowerCase();
+      const caption = photos[i].getAttribute('data-caption');
+
       photos[i].style.display = 'none';
-    const caption = photos[i].getAttribute('data-caption');
-    if(caption.includes(e.target.value)){
+      
+    if(caption.includes(userInput)){
       photos[i].style.display = '';
-    }     
+      noMatches = false;
+    } else {
+      noMatches = true;
+    }
   }
+
+  // if(noMatches) {
+  //   console.log("NO MATCHES")
+  //   const noResults = document.createElement('p');
+  //   noResults.innerHTML = `No Search Results Were Found. Try Another Input`;
+  //   gallery.insertAdjacentHTML('afterbegin', noResults);
+  // } else {
+  //   noResults.remove();
+  // }
 
 
 
